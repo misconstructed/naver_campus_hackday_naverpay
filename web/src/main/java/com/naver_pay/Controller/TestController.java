f7d894f8d73cc3ebc0ea6d3872bd02ad8e4f24d8
Hyuhng Joon Kim <heyjoonkim@gmail.com>
2018-11-23 17:18:35 +0900

    Spring Boot default setting

---
+++ web/src/main/java/com/naver_pay/Controller/TestController.java
@@ -0,0 +1,14 @@
+package com.naver_pay.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test print";
+    }
+
+}
